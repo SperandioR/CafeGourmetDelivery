ad44a991a49dc6292e440e017babd9c4233a226b
SperandioR <rodrigopaulino13@gmail.com>
2024-09-08 12:36:33 -0300

    Adicionar arquivos de projeto.

---
+++ CafeGourmetDelivery/Controllers/CafeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CafeGourmetDelivery.Controllers
+{
+    public class CafeController : Controller
+    {
+        // GET: Cafe
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CafeGourmetDelivery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CafeGourmetDelivery.MvcApplication" Language="C#" %>
