ee321d2e66aca1d287f411f69934bec40c67d25a
SperandioR <rodrigopaulino13@gmail.com>
2024-09-08 12:41:37 -0300

    Adicionando as seguintes ações ao CafeController, Pag Inicial, Pag Menu, Pagina sobre nos, pagina de contato

--- CafeGourmetDelivery/Controllers/CafeController.cs
+++ CafeGourmetDelivery/Controllers/CafeController.cs
@@ -13,5 +13,24 @@ namespace CafeGourmetDelivery.Controllers
         {
             return View();
         }
+
+        // Página do menu
+        public ActionResult Menu()
+        {
+            // Aqui você pode adicionar lógica para buscar itens de um banco de dados
+            return View();
+        }
+
+        // Página sobre nós
+        public ActionResult Sobre()
+        {
+            return View();
+        }
+
+        // Página de contato
+        public ActionResult Contato()
+        {
+            return View();
+        }
     }
 }
