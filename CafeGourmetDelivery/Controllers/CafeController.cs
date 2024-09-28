00ca6a3bb391ed820a9c5a9cc2efb163db2943d1
SperandioR <rodrigopaulino13@gmail.com>
2024-09-28 17:35:10 -0300

    criando métodos e corrigindo bugs na pagina de pedidos.

--- CafeGourmetDelivery/Controllers/CafeController.cs
+++ CafeGourmetDelivery/Controllers/CafeController.cs
@@ -37,6 +37,33 @@ namespace CafeGourmetDelivery.Controllers
         public ActionResult Pedido(string nomeProduto)
         {
             ViewBag.NomeProduto = nomeProduto;
+
+            // Definindo a imagem com base no nome do produto recebido
+            switch (nomeProduto)
+            {
+                case "Café Expresso":
+                    ViewBag.CaminhoImagem = "~/Content/imagens/copodecafe.png";
+                    break;
+                case "Café Latte":
+                    ViewBag.CaminhoImagem = "~/Content/imagens/cafecomleite.png";
+                    break;
+                case "Cappuccino":
+                    ViewBag.CaminhoImagem = "~/Content/imagens/cappuccino.png";
+                    break;
+                case "Bolo de Cenoura":
+                    ViewBag.CaminhoImagem = "~/Content/imagens/bolocenoura.jpg";
+                    break;
+                case "Cookies de Chocolate":
+                    ViewBag.CaminhoImagem = "~/Content/imagens/Cookies.png";
+                    break;
+                case "Croissant":
+                    ViewBag.CaminhoImagem = "~/Content/imagens/croissant.png";
+                    break;
+                default:
+                    ViewBag.CaminhoImagem = "~/Content/imagens/produto-exemplo.png";
+                    break;
+            }
+
             return View();
         }
 
@@ -47,6 +74,7 @@ namespace CafeGourmetDelivery.Controllers
             ViewBag.Mensagem = $"Pedido confirmado: {quantidade}x {nomeProduto}. Obrigado por comprar conosco!";
             return View("Confirmacão");
         }
+
 
     }
 }
