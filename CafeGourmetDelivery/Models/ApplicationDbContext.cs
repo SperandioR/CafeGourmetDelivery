08435f7ac6f82d96a82460c58cbc7854713aa5d8
SperandioR <rodrigopaulino13@gmail.com>
2024-10-13 20:03:02 -0300

    Criando uma classe chamado Produto, com isso, uma tabela Produtos será mapeada para a classe Produto, permitindo que o Entity Framework Core realize operações de CRUD (Create, Read, Update, Delete) diretamente na tabela.

---
+++ CafeGourmetDelivery/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CafeGourmetDelivery.Models;
+using System.Data.Entity;
+
+public class ApplicationDbContext : DbContext
+{
+    // Construtor que chama o DbContext base com a string de conexão
+    public ApplicationDbContext() : base("name=DefaultConnection")
+    {
+    }
+
+    // DbSet para as entidades (tabelas) do seu banco de dados
+    public DbSet<Produto> Produtos { get; set; }
+    // Adicione outras entidades aqui, como:
+    // public DbSet<Cliente> Clientes { get; set; }
+}
---
+++ CafeGourmetDelivery/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CafeGourmetDelivery.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string NomeProduto { get; set; }
+        public string DescricaoProduto { get; set; }
+        public decimal Preco { get; set; }
+        public string CaminhoImagem { get; set; }
+    }
+}
