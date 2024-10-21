6d1f7756bd2d584265a13a378ec56fe214ce0760
SperandioR <rodrigopaulino13@gmail.com>
2024-10-21 18:04:36 -0300

    Criando o controlador ClienteController que será responsável por exibir a página de login e gerenciar o cadastro de novos clientes.

---
+++ CafeGourmetDelivery/Controllers/ClienteController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace CafeGourmetDelivery.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: Cliente/Cadastrar
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
